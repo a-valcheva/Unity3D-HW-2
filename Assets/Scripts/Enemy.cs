305ef4f47b38ec240529595172e732812bd070cd
anichka777 <avalcheva7@gmail.com>
2014-12-01 22:45:38 +0200

    homework

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public Transform target;
+	float posX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (target);
+
+		float dist = Vector3.Distance (target.position, transform.position);
+		if (dist.CompareTo (3f) < 0f) {
+			// TODO to see lights on the cube when it is shooting
+
+			//Player to feel the shooting/ to shake
+			posX = Mathf.Sin(Time.deltaTime * 2f);
+			target.transform.position = new Vector3(posX, target.transform.position.y, target.transform.position.z);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public Camera mainCam;
+	public float looksensitivity = 5f;
+	public float lookSmoothDamp = 0.1f;
+	float yrotation;
+	float xrotation;
+	float Currentyrotation;
+	float Currentxrotation;
+	float yRotaionV;
+	float xRotationV;
+	float speed = 5f;
+	
+	void Update () {
+		// Player moves without walking
+		yrotation += Input.GetAxis ("Mouse X") * looksensitivity;
+		xrotation -= Input.GetAxis ("Mouse Y") * looksensitivity;
+		
+		xrotation = Mathf.Clamp (xrotation, -90, 90);
+		
+		Currentxrotation = Mathf.SmoothDamp (Currentxrotation, xrotation, ref xRotationV, lookSmoothDamp);
+		Currentyrotation = Mathf.SmoothDamp (Currentyrotation, yrotation, ref yRotaionV, lookSmoothDamp);
+		
+		mainCam.transform.rotation = Quaternion.Euler (xrotation, yrotation, 0);
+		transform.rotation = Quaternion.Euler (0, yrotation, 0);
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			transform.Translate(Vector3.left*Time.deltaTime*speed);
+		} 
+		if (Input.GetKey(KeyCode.RightArrow)) 
+		{
+			transform.Translate(Vector3.right*Time.deltaTime*speed);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) 
+		{
+			transform.Translate(Vector3.forward*Time.deltaTime*speed);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) 
+		{
+			transform.Translate(Vector3.back*Time.deltaTime*speed);
+		}
+		
+	}
+}﻿
+
